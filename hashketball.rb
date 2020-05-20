7c2387041c92ce4cf7debe727339ad71237d9447
Danira Cortez <cortezd334@gmail.com>
2020-05-20 05:21:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -38,28 +38,28 @@ def game_hash
         steals: 3,
         blocks: 1,
         slam_dunks: 15
-        },
-        {
-        player_name: "",
-        number: ,
-        shoe: ,
-        points: ,
-        rebounds: ,
-        assists: ,
-        steals: ,
-        blocks: ,
-        slam_dunks: 
-        },
-        {
-        player_name: "",
-        number: ,
-        shoe: ,
-        points: ,
-        rebounds: ,
-        assists: ,
-        steals: ,
-        blocks: ,
-        slam_dunks: 
+        # },
+        # {
+        # player_name: "",
+        # number: ,
+        # shoe: ,
+        # points: ,
+        # rebounds: ,
+        # assists: ,
+        # steals: ,
+        # blocks: ,
+        # slam_dunks: 
+        # },
+        # {
+        # player_name: "",
+        # number: ,
+        # shoe: ,
+        # points: ,
+        # rebounds: ,
+        # assists: ,
+        # steals: ,
+        # blocks: ,
+        # slam_dunks: 
         }]
     },
 
